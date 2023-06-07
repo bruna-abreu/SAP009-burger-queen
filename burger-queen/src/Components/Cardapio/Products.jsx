c26a22e732d7a5253e3de50f77f2d4664526ac01
Polyana <polyanacristina2001@gmail.com>
2023-06-07 16:35:54 -0300

    filtro e resume de pedidos

--- burger-queen/src/Components/Cardapio/Products.jsx
+++ burger-queen/src/Components/Cardapio/Products.jsx
@@ -1,11 +1,18 @@
+import React from "react";
+
 function Products(props) {
-    return (
-        <div className={props.squareClassName}>
-            <button type="submit" onClick={props.onClick} className={props.itemClassName}>
-                <p className={props.productClassName} id={props.productNameId}>{props.item}</p>
-                <p className={props.productClassName} id={props.productPriceId}>$ {props.price}</p>
-            </button>
-        </div>
-      );
+  const handleClick = () => {
+    props.addProductToOrder(props.item);
+  };
+
+  return (
+    <div className={props.squareClassName}>
+      <button type="submit" onClick={handleClick} className={props.itemClassName}>
+        <p className={props.productClassName} id={props.productNameId}>{props.item.name}</p>
+        <p className={props.productClassName} id={props.productPriceId}>$ {props.item.price}</p>
+      </button>
+    </div>
+  );
 }
+
 export default Products;
