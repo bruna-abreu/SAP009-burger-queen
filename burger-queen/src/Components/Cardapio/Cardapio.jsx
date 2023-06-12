5bdffdbe5a3aa8620b2cb05bebea1cb45b83af36
Polyana <polyanacristina2001@gmail.com>
2023-06-12 16:39:06 -0300

    soma dos produtos

--- burger-queen/src/Components/Cardapio/Cardapio.jsx
+++ burger-queen/src/Components/Cardapio/Cardapio.jsx
@@ -1,65 +1,100 @@
-import { useState, useEffect } from "react";
-import Btn from '../Btn/Btn'
-import Input from '../Input/Input'
-import Products from "./Products";
-import ProductsBox from "./ProductsBox";
-import getProducts from "../../API/menuProducts"
-import './Cardapio.css'
+import React, { useState, useEffect } from 'react';
+import Btn from '../Btn/Btn';
+import Input from '../Input/Input';
+import Products from './Products';
+import ProductsBox from './ProductsBox';
+import getProducts from '../../API/menuProducts';
+import './Cardapio.css';
 
 function Cardapio() {
-  const [cliente, setCliente] = useState('')
-  const [mesa, setMesa] = useState('')
-  const [products, setProducts] = useState([])
-  const [filteredProducts, setFilteredProducts] = useState([])
-  const [activeBtn, setActiveBtn] = useState('cafeDaManha')
-  const [orderSummary, setOrderSummary] = useState([])
+  const [cliente, setCliente] = useState('');
+  const [mesa, setMesa] = useState('');
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [activeBtn, setActiveBtn] = useState('cafeDaManha');
+  const [orderSummary, setOrderSummary] = useState([]);
 
   useEffect(() => {
     async function listProducts() {
-      const productsList = await getProducts()
-      setProducts(productsList)
+      const productsList = await getProducts();
+      setProducts(productsList);
 
       // Filtrar os produtos do café da manhã
-      const cafeDaManhaProducts = productsList.filter(product => product.type === 'Café da manhã')
-      setFilteredProducts(cafeDaManhaProducts)
+      const cafeDaManhaProducts = productsList.filter(product => product.type === 'Café da manhã');
+      setFilteredProducts(cafeDaManhaProducts);
     }
-    listProducts()
-  }, [])
+    listProducts();
+  }, []);
 
   useEffect(() => {
     // Atualizar os produtos filtrados quando o botão ativo for alterado
     if (activeBtn === 'cafeDaManha') {
-      const cafeDaManhaProducts = products.filter(product => product.type === 'Café da manhã')
-      setFilteredProducts(cafeDaManhaProducts)
+      const cafeDaManhaProducts = products.filter(product => product.type === 'Café da manhã');
+      setFilteredProducts(cafeDaManhaProducts);
     } else if (activeBtn === 'principal') {
-      const principalProducts = products.filter(product => product.type === 'Principal')
-      setFilteredProducts(principalProducts)
+      const principalProducts = products.filter(product => product.type === 'Principal');
+      setFilteredProducts(principalProducts);
     }
-  }, [activeBtn, products])
+  }, [activeBtn, products]);
 
-  // Adicionar um produto ao resumo do pedido
   const addProductToOrder = (product) => {
-    setOrderSummary(prevOrder => [...prevOrder, product])
-  }
+    const existingProduct = orderSummary.find(item => item.id === product.id);
 
-  // Remover um produto do resumo do pedido
-  const removeProductFromOrder = (product) => {
-    setOrderSummary(prevOrder => prevOrder.filter(item => item.id !== product.id))
-  }
+    if (existingProduct) {
+      const updatedOrder = orderSummary.map(item => {
+        if (item.id === product.id) {
+          return { ...item, quantity: item.quantity + 1 };
+        }
+        return item;
+      });
+      setOrderSummary(updatedOrder);
+    } else {
+      setOrderSummary(prevOrder => [...prevOrder, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const removeProductFromOrder = (productId) => {
+    const updatedOrder = orderSummary.filter(item => item.id !== productId);
+    setOrderSummary(updatedOrder);
+  };
+
+  const increaseProductQuantity = (productId) => {
+    const updatedOrder = orderSummary.map(item => {
+      if (item.id === productId) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    setOrderSummary(updatedOrder);
+  };
+
+  const decreaseProductQuantity = (productId) => {
+    const updatedOrder = orderSummary.map(item => {
+      if (item.id === productId && item.quantity > 1) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
+    setOrderSummary(updatedOrder);
+  };
 
   return (
     <>
       <div className="btnCardapio">
         <Btn
-          buttonClassName={`cafeDaManhaBtn ${activeBtn === 'cafeDaManha' ? 'active' : ''}`}
+          buttonClassName={`cafeDaManhaBtn ${
+            activeBtn === 'cafeDaManha' ? 'active' : ''
+          }`}
           btnTextClassName="btnTextCardapio"
           id="cafeDaManha"
           text="CAFÉ DA MANHÃ"
           path="/Cardapio"
           onClick={() => setActiveBtn('cafeDaManha')}
-        />  
+        />
         <Btn
-          buttonClassName={`principalBtn ${activeBtn === 'principal' ? 'active' : ''}`}
+          buttonClassName={`principalBtn ${
+            activeBtn === 'principal' ? 'active' : ''
+          }`}
           btnTextClassName="btnTextCardapio"
           id="principal"
           text="PRINCIPAL"
@@ -67,10 +102,10 @@ function Cardapio() {
           onClick={() => setActiveBtn('principal')}
         />
       </div>
-      <div className="inputCardapio"> 
+      <div className="inputCardapio">
         <Input
           value={cliente}
-          whenChanged={(value) => setCliente(value)}  
+          whenChanged={(value) => setCliente(value)}
           type="text"
           id="inputCliente"
           placeholder="Nome do cliente"
@@ -86,34 +121,33 @@ function Cardapio() {
         />
       </div>
       <div className="containerCardapio">
-        <div className="teste">
-          <div className="productsCardapio">
+        <div className='teste'>
+          <div className='productsCardapio'>
             {filteredProducts.map((product) => (
-                <Products
-                key={product.id}
-                squareClassName="squareProducts"
-                itemClassName="productsBtn"
-                productClassName="productsInfo"
-                productNameId="productName"
-                productPriceId="productPrice"
-                item={product}
-                addProductToOrder={addProductToOrder}
+              <Products
+              key={product.id}
+              squareClassName="squareProducts"
+              itemClassName="productsBtn"
+              productClassName="productsInfo"
+              productNameId="productName"
+              productPriceId="productPrice"
+              item={product}
+              addProductToOrder={addProductToOrder}
               />
             ))}
           </div>
         </div>
           <section>
             <ProductsBox
-              products={filteredProducts}
-              orderSummary={orderSummary}
-              removeProductFromOrder={removeProductFromOrder}
+            orderSummary={orderSummary}
+            removeProductFromOrder={removeProductFromOrder}
+            increaseProductQuantity={increaseProductQuantity}
+            decreaseProductQuantity={decreaseProductQuantity}
             />
           </section>
-       </div>
-      
-      </>
-    );
-  }
-  
-  export default Cardapio;
-  
+        </div>
+        </>
+      );
+    }
+
+export default Cardapio;
--- burger-queen/src/Components/Cardapio/Products.jsx
+++ burger-queen/src/Components/Cardapio/Products.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+// import React from "react";
 
-function Products(props) {
+// function Products(props) {
+//   const handleClick = () => {
+//     props.addProductToOrder(props.item);
+//   };
+
+//   return (
+//     <div className={props.squareClassName}>
+//       <button type="submit" onClick={handleClick} className={props.itemClassName}>
+//         <p className={props.productClassName} id={props.productNameId}>{props.item.name}</p>
+//         <p className={props.productClassName} id={props.productPriceId}>$ {props.item.price}</p>
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default Products;
+
+import React from 'react';
+
+export default function Products({ item, addProductToOrder }) {
   const handleClick = () => {
-    props.addProductToOrder(props.item);
+    addProductToOrder(item);
   };
 
   return (
-    <div className={props.squareClassName}>
-      <button type="submit" onClick={handleClick} className={props.itemClassName}>
-        <p className={props.productClassName} id={props.productNameId}>{props.item.name}</p>
-        <p className={props.productClassName} id={props.productPriceId}>$ {props.item.price}</p>
+    <div className="squareProducts">
+      <button type="submit" onClick={handleClick} className="productsBtn">
+        <p className="productsInfo" id="productName">{item.name}</p>
+        <p className="productsInfo" id="productPrice">$ {item.price}</p>
       </button>
     </div>
   );
 }
 
-export default Products;
+
