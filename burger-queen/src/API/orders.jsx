7e8aec215aac329d9ceadf6edc3c4a1b80f991a2
Polyana <polyanacristina2001@gmail.com>
2023-06-20 14:41:03 -0300

    enviar os pedidos para a cozinha

--- burger-queen/src/API/orders.jsx
+++ burger-queen/src/API/orders.jsx
@@ -1,4 +1,5 @@
-const API_URL = 'https://burger-queen-api-mock-sand.vercel.app';
+// const API_URL = 'https://burger-queen-api-mock-sand.vercel.app';
+const API_URL = 'http://localhost:8080'
 
 const createOrder = async(userId, clientName, selectValue, orderResume, token) => await fetch(`${API_URL}/orders`, {
 method: 'POST',
@@ -16,12 +17,16 @@ body: JSON.stringify({
 })
 });
 
-const showOrders = async (token) => fetch(`${API_URL}/orders`, {
+const showOrders = async (token) => { 
+  const response = await fetch(`${API_URL}/orders`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
   });
+  console.log(response)
+  return response.json()
+}
 
 export {createOrder, showOrders};
